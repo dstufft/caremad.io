f899de388841e32ac5b157ee40f9ea64c1b3387b
Donald Stufft <donald@stufft.io>
2013-07-15 21:32:27 -0400

    Add configuration for Pelican

---
+++ conf/defaults.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = "Donald Stufft"
+SITENAME = "caremad"
+SITEURL = ""
+
+TIMEZONE = "America/New_York"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = [
+    ("Pelican", "http://getpelican.com/"),
+    ("Python.org", "http://python.org/"),
+    ("Jinja2", "http://jinja.pocoo.org/"),
+    ("You can modify those links in your config file", "#"),
+]
+
+# Social widget
+SOCIAL = [
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+]
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+DIRECT_TEMPLATES = ["index"]
+
+ARTICLE_URL = "blog/{slug}/"
+ARTICLE_SAVE_AS = "blog/{slug}/index.html"
+
+AUTHOR_SAVE_AS = False
+CATEGORY_SAVE_AS = False
+TAG_SAVE_AS = False
+
+FILENAME_METADATA = "(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)"
+
+
+THEME = "themes/caremad"
+
+
+PLUGIN_PATH = "plugins"
+PLUGINS = [
+    "assets",
+]
---
+++ conf/publish.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+import os.path
+import sys
+
+sys.path = [os.path.dirname(__file__)] + sys.path
+
+from defaults import *
+
+SITEURL = 'https://caremad.io'
+RELATIVE_URLS = False
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
