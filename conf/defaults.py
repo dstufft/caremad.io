e3c4e96f1cf09cd7f00814ae14641bcc619b3234
Donald Stufft <donald@stufft.io>
2013-07-18 00:52:22 -0400

    Add an index template which includes a project listing

--- conf/defaults.py
+++ conf/defaults.py
@@ -18,24 +18,15 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
-LINKS = [
-    ("Pelican", "http://getpelican.com/"),
-    ("Python.org", "http://python.org/"),
-    ("Jinja2", "http://jinja.pocoo.org/"),
-    ("You can modify those links in your config file", "#"),
-]
-
-# Social widget
-SOCIAL = [
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+# Projects
+PROJECTS = [
+    (
+        "Warehouse", "https://github.com/dstufft/warehouse",
+        "Next Generation Python Package Index",
+    ),
 ]
 
 DEFAULT_PAGINATION = False
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
 
 DIRECT_TEMPLATES = ["index"]
 
