00b06d17efe26f1cdb9fddea09e922d933d5f046
Donald Stufft <donald@stufft.io>
2013-07-18 22:41:04 -0400

    Setup the Feed urls for generation

--- conf/publish.py
+++ conf/publish.py
@@ -7,10 +7,11 @@ sys.path = [os.path.dirname(__file__)] + sys.path
 
 from defaults import *
 
-SITEURL = 'https://caremad.io'
+SITEURL = "https://caremad.io"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/%s.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
